3570ddcdb4e0175c4e241cb87928e7b8cbd97eab
Gen709 <709ruebeaudoin@gmail.com>
2022-08-21 15:04:16 -0400

    student premier model

--- student/models.py
+++ student/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    nom = models.CharField(max_length=200)
+    prenom = models.CharField(max_length=200)
+    comite_clinique = models.BooleanField(null=True)
+    date_ref_comite_clinique = models.DateField(null=True)
+    plan_intervention = models.BooleanField(default=False)
+    groupe_repere = models.CharField(max_length=4)
+    
