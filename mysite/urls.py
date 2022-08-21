9bd5645ea88751fa1efd8b4135d9df4f5f4a3d05
Gen709 <709ruebeaudoin@gmail.com>
2022-08-21 17:58:09 -0400

    ajout problematiques a mysite/urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     
     path('etudiant/', include('student.urls')),
+    path('problematiques/', include('problematiques.urls')),
     
 
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
