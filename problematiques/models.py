2ff4c9c6af0d9dacc4f7e08418480d09a189eddd
Gen709 <709ruebeaudoin@gmail.com>
2022-08-21 19:22:09 -0400

    ajout problematiques

--- problematiques/models.py
+++ problematiques/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
+from student.models import Student
+
 # Create your models here.
 
 class Categorie(models.Model):
     nom = models.CharField(max_length=200)
     
+    def __str__(self):
+        return self.nom
+    
 class Item(models.Model):
     nom = models.CharField(max_length=200)
     categorie = models.ForeignKey(Categorie, on_delete=models.SET_NULL, null=True)
+    student = models.ManyToManyField(Student)
+    
+    def __str__(self):
+        return self.nom +" (" + self.categorie.nom + ")"
