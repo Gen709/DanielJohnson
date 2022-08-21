84a4ecbd4ae63493b8c5108caad52a6adeef3da4
Gen709 <709ruebeaudoin@gmail.com>
2022-08-21 15:12:46 -0400

    student change bool from null to default false

--- student/models.py
+++ student/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Student(models.Model):
     nom = models.CharField(max_length=200)
     prenom = models.CharField(max_length=200)
-    comite_clinique = models.BooleanField(null=True)
+    comite_clinique = models.BooleanField(default=False)
     date_ref_comite_clinique = models.DateField(null=True)
     plan_intervention = models.BooleanField(default=False)
     groupe_repere = models.CharField(max_length=4)
