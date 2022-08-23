e7fb55f202ee576d1abce4bb32980992f11520d5
Gen709 <709ruebeaudoin@gmail.com>
2022-08-23 22:20:49 +0000

    test

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,9 +22,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    
+
     path('etudiant/', include('student.urls')),
     path('problematiques/', include('problematiques.urls')),
-    
+    path('', include('page.urls')),
+
 
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
