273cca61efdf204fd00e6c0da6b7e2d4f3c9400f
Gen709 <709ruebeaudoin@gmail.com>
2022-09-07 12:28:48 -0400

    du owner à  l'app school

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Classification
+ 
+model_list = [Classification]
+
+# Register your models here.
+
+for model in model_list:
+    admin.site.register(model)
