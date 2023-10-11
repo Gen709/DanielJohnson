4f18c972d3da60458792ba00e18046bc2ef94395
Gen709 <709ruebeaudoin@gmail.com>
2023-10-11 10:53:45 -0400

    site/urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,16 +17,22 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
+from teacher.views import CustomLoginView
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-
-    path('etudiant/', include('student.urls')),
+    path('', CustomLoginView.as_view(), name='login'),
     path('problematiques/', include('problematiques.urls')),
-    path('', include('page.urls')),
-    path('', include('school.urls')),
+    path('page/', include('page.urls')),
+    path('school/', include('school.urls')),
+    path('teacher/', include('teacher.urls')),
+    path('etudiant/', include('student.urls')),
     # path('user/profile/<pk:int>')
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
