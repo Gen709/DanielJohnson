86f1564fc7424f9b8c299d9327632acee36111a0
Gen709 <709ruebeaudoin@gmail.com>
2023-10-11 10:57:50 -0400

    test

--- page/urls.py
+++ page/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'), # this redirect to user/profile
+    path('gyy', views.index, name='index'), # this redirect to user/profile
     path('user/profile/<int:pk>', views.user_detail, name='home'),
     # path('user/task/<int:pk>', views.user_task, name='user-task'),
     path('etudiant/ajax/etatdelasituation/', views.ajax_etat_situation_save_view, name='etat-situation-save')
