a463f59fa65c4078cbb91dccd30a5836d61c2f79
Gen709 <709ruebeaudoin@gmail.com>
2023-10-12 10:32:16 -0400

    teacher

--- teacher/admin.py
+++ teacher/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import RegularTeacher, Professional, SchoolAdmin
+from .models import RegularTeacher, Professional, SchoolAdmin, SpecialtyTeacher
 
-model_list = [RegularTeacher, Professional, SchoolAdmin]
+model_list = [RegularTeacher, Professional, SchoolAdmin, SpecialtyTeacher]
 
 for model in model_list:
     admin.site.register(model)
--- teacher/models.py
+++ teacher/models.py
@@ -7,6 +7,9 @@ from school.models import Group
 class SchoolAdmin(User):
     resp = models.IntegerField(default=None, null=True, blank=True)
 
+    @property
+    def get_teacher(self):
+        self.classification
 
 class RegularTeacher(User):
     group = models.OneToOneField(Group, on_delete=models.SET_NULL, null=True)
@@ -27,17 +30,44 @@ class RegularTeacher(User):
 
     def __str__(self):
         return self.first_name + " " + self.last_name 
+    
+    class Meta:
+        verbose_name = 'Enseignant Généraliste'
+        verbose_name_plural = 'Enseignants Généralistes'
 
 
 class SpecialtyTeacher(User):
     all_groups = models.ManyToManyField(Group)
     matière = models.CharField(max_length=30, blank=True, null=True)
 
+    def delete(self, *args, **kwargs):
+            # Delete the associated user
+            self.user.delete()
+            # Call the parent class's delete method to delete the subclass instance
+            super().delete(*args, **kwargs)
+
     def __str__(self):
-        return self.name
+        return self.name + " " + self.matière
+    
+    class Meta:
+        verbose_name = 'Enseignant Spécialisé'
+        verbose_name_plural = 'Enseignants Spécialisés'
 
    
 class Professional(User):
     speciality = models.CharField(max_length=100, blank=True, null=True, default=None)
     local =  models.CharField(max_length=30, blank=True, null=True, default=None)
+
+    def delete(self, *args, **kwargs):
+            # Delete the associated user
+            self.user.delete()
+            # Call the parent class's delete method to delete the subclass instance
+            super().delete(*args, **kwargs)
+
+    def __str__(self):
+        return self.get_full_name() + " (" + self.speciality + ")"
+
+    class Meta:
+            verbose_name = 'Professionel'
+            verbose_name_plural = 'Professionels'
     
